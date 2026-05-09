77f8df3c23ce2344f19d6822b609cfb9ca59c664
h-kido <kido@ks-network.jp>
2026-05-09 23:40:23 +0900

    DBクリア

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class Message(db.Model):
 with app.app_context():
     try:
         # 【重要】古い不完全なテーブルを一度確実に消去
-        #db.drop_all()
+        db.drop_all()
         db.create_all()
         print("Database re-initialized.")
     except Exception as e:
